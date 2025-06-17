c060877864cc42a869d642f32b2e8cae73763fe1
SergioLuis <daw202515@estudiantes.salesianasnsp.es>
2025-06-17 19:27:35 +0200

    cambios lecciones

--- src/app/componentes/lecciones/lecciones.component.ts
+++ src/app/componentes/lecciones/lecciones.component.ts
@@ -19,7 +19,7 @@ export class LeccionesComponent {
   usu?: IProgreso;
   id: number = 0;
   idUsu: number = 0;
-  idNivel: number = 0
+  idNivel: number = 0;
 
   constructor(
     private route: Router,
@@ -30,51 +30,59 @@ export class LeccionesComponent {
     this.activatedRouter.paramMap.subscribe((params: ParamMap) => {
       const idParam = params.get('id') || '';
       this.id = idParam ? Number(idParam) : 0;
-    });
-
-    this.idUsu = this.login.retornarId();
-     console.log('Parametro recibido:'+this.id)
-    this.data.getAllLecciones(this.id).subscribe((a) => {
-      a.forEach((e) => {
-        this.lecciones.push(e);
-      });
-      console.log(this.lecciones)
-      this.getUsuPro(this.lecciones[0].curso_id)
-      this.idNivel=this.lecciones[0].curso_id
+      this.idUsu = this.login.retornarId();
+      console.log('Parametro recibido:' + this.id);
+      this.recargarDatos();
     });
   }
 
-  getUsuPro(idCurso: number){
+  recargarDatos() {
+    this.lecciones = [];
+
+    this.data.getAllLecciones(this.id).subscribe((a) => {
+      this.lecciones = a;
+      console.log(this.lecciones);
+      if (this.lecciones.length > 0) {
+        this.idNivel = this.lecciones[0].curso_id;
+        this.getUsuPro(this.lecciones[0].curso_id);
+      }
+    });
+  }
+
+  getUsuPro(idCurso: number) {
     this.data.getUsuarioProgreso(this.idUsu, idCurso).subscribe((a) => {
       this.usu = a;
       console.log('usuario: ', this.usu);
-      if(this.usu?.leccion_id>=this.lecciones.length + 1)
-          this.subirNivel(this.usu.usuario_id, this.usu.curso_id)
-    });
-    
-}
-subirNivel(idUsu:number, idCurso:number){
-      this.login.sumarPuntosNivel(this.idUsu)
-      .subscribe({
-        next: (respuesta) => {
-          console.log('Edicion exitosa:', respuesta);
-        },
-        error: (error) => {
-          console.error('Error al editar:', error);
-        }
-      });   
-
-
-        this.data.UpdateNivelUsuarioCurso(idUsu, idCurso).subscribe({
-        next: (res: any) => {
-          console.log("Mensaje:", res.mensaje);
-        },
-      error: (err) => {
-        console.error("Error al updatear:", err);
+      if (this.usu?.leccion_id >= this.lecciones.length + 1) {
+        this.subirNivel(this.usu.usuario_id, this.usu.curso_id);
       }
     });
-    alert('Has completado todas las lecciones del nivel! se han sumado... 100 puntos!');
-    }
+  }
+
+  subirNivel(idUsu: number, idCurso: number) {
+    this.login.sumarPuntosNivel(this.idUsu).subscribe({
+      next: (respuesta) => {
+        console.log('Edicion exitosa:', respuesta);
+
+        this.data.UpdateNivelUsuarioCurso(idUsu, idCurso).subscribe({
+          next: (res: any) => {
+            console.log('Mensaje:', res.mensaje);
+            alert(
+              '¡Has completado todas las lecciones del nivel! Se han sumado 100 puntos.'
+            );
+            this.recargarDatos(); // Recarga los datos para reflejar el progreso
+          },
+          error: (err) => {
+            console.error('Error al updatear:', err);
+          },
+        });
+      },
+      error: (error) => {
+        console.error('Error al editar:', error);
+      },
+    });
+  }
+
   seleccionarLeccion(leccion: any) {
     console.log('Lección seleccionada:', leccion);
     const nombreLimpio = leccion.nombre
@@ -82,18 +90,16 @@ subirNivel(idUsu:number, idCurso:number){
       .normalize('NFD')
       .replace(/[\u0300-\u036f]/g, '');
 
-    // Usa las propiedades correctas del objeto lección
     this.route.navigate([
       '/home/info-lecciones',
-      leccion.id, // Cambiado de leccion.leccion_id a leccion.id
-      leccion.curso_id, // Cambiado de leccion.nivel_id a leccion.nivelId
+      leccion.id,
+      leccion.curso_id,
       nombreLimpio,
     ]);
   }
-  atras(){
-    console.log("Id del nivel: "+this.idNivel)
-       this.route.navigate([
-      '/home/curso-niveles/'+this.idNivel
-    ]);
+
+  atras() {
+    console.log('Id del nivel: ' + this.idNivel);
+    this.route.navigate(['/home/curso-niveles/' + this.idNivel]);
   }
 }
