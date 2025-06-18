3118ad484766a934c2bbfdc3114eb40613669674
Alberto Barbacid <daw202502@estudiantes.salesianasnsp.es>
2025-06-18 15:58:32 +0200

    back

--- src/app/enviroments/enviroments.ts
+++ src/app/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://localhost:7051/',
-  apiUrl: 'https://dawlingo-back.fly.dev/api',
+  apiUrl: 'dawlingo-back-6q1d.vercel.app',
 };
