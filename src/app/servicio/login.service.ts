a39f8caba9823fd93a7c398a952c761f9acd24a7
Alberto Barbacid <daw202502@estudiantes.salesianasnsp.es>
2025-06-18 16:18:26 +0200

    a

--- src/app/servicio/login.service.ts
+++ src/app/servicio/login.service.ts
@@ -19,7 +19,7 @@ export class LoginService {
     };
     console.log(environment.apiUrl)
     return this.http
-      .post('https://dawlingo-2fcl.vercel.app/Login/Login', body)
+      .post(environment.apiUrl + '/Login/Login', body)
       .pipe(shareReplay());
   }
 
